e867ea260e326b910f8438b8ef598c17d6af125d
robert-virtual <robertocastill63@gmail.com>
2023-02-22 08:31:58 -0600

    update course adding students to it

--- src/main/java/com/example/udemy_backend/repository/StudentCourseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.udemy_backend.repository;
-
-
-import com.example.udemy_backend.model.StudentCourse;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentCourseRepository extends JpaRepository<StudentCourse,Long> {
-}
--- src/main/java/com/example/udemy_backend/service/StudentService.java
+++ src/main/java/com/example/udemy_backend/service/StudentService.java
@@ -5,9 +5,7 @@ import com.example.udemy_backend.model.Course;
 import com.example.udemy_backend.model.Student;
 import com.example.udemy_backend.model.StudentCourse;
 import com.example.udemy_backend.repository.CourseRepository;
-import com.example.udemy_backend.repository.StudentCourseRepository;
 import com.example.udemy_backend.repository.StudentRepository;
-import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,8 +13,6 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class StudentService {
     private final StudentRepository studentRepo;
-    private final EntityManager entityManager;
-    private final StudentCourseRepository studentCourseRepo;
     private final CourseRepository courseRepo;
     public Iterable<Student> all(){
         return studentRepo.findAll();
@@ -27,23 +23,16 @@ public class StudentService {
     }
 
     public Course addCourse(StudentCourse body) {
-        // TODO: add course to student
+         return courseRepo.findById(body.getCourse_id()).map(course -> {
+             course.setName(course.getName()+" (updated)");
+             Student student = studentRepo.findById(body.getStudent_id()).orElseThrow();
+             course.addStudent(student);
+             return courseRepo.save(course);
+         }).orElseThrow();
 
-//         return courseRepo.findById(body.getCourse_id()).map(course->{
-//                    course.addStudent(studentRepo.findById(body.getStudent_id()).orElseThrow());
-//                    return course;
-//        }).orElseThrow();
-        // entityManager.find()
-        Course course = entityManager.find(Course.class,body.getCourse_id());
-        Student student = entityManager.find(Student.class,body.getStudent_id());
-        course.addStudent(student);
-        entityManager.persist(course);
-        return course;
     }
 
     public Iterable<CoursesDto> getCourses(long student_id) {
-        // Student student = studentRepo.findById(id).orElseThrow();
-        Iterable<CoursesDto> res = courseRepo.getCourses(student_id);
-        return res;
+        return courseRepo.getCourses(student_id);
     }
 }
